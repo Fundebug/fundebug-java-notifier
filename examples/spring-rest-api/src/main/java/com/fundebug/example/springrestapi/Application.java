1a4582569ac01ef7edd4131b9bf0ee318346378d
kiwenlau <kiwenlau@126.com>
2018-12-30 21:45:48 +0800

    package name只能用小写字母

---
+++ examples/spring-rest-api/src/main/java/com/fundebug/example/springrestapi/Application.java
@@ -0,0 +1,11 @@
+package com.fundebug.example.springrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
